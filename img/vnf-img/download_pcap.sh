18ee16b522b23aadacb2bf5771f3ebede18b7a4a
David Martínez García <94689492+martinezgarciadavid@users.noreply.github.com>
2022-12-29 11:40:46 +0100

    Add files via upload

---
+++ img/vnf-img/download_pcap.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget https://github.com/martinezgarciadavid/nfv-lab-cds/releases/download/v1.0.0/capture+crypto+type2+8+vlan.pcap
---
+++ vnx/vnx_create_all.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo vnx -f nfv3_home_lxc_ubuntu64.xml --create && sudo vnx -f nfv3_server_lxc_ubuntu64.xml --create
