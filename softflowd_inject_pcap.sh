91f8408cb14fe9086f13289a42cf23ff0c7ced9c
David Martínez García <94689492+martinezgarciadavid@users.noreply.github.com>
2022-12-29 12:33:49 +0100

    Scripts updated

--- softflowd_inject_pcap.sh
+++ softflowd_inject_pcap.sh
@@ -20,5 +20,5 @@ VCPE="deploy/$OSMCPE"
 CPE_EXEC="$KUBECTL exec -n $OSMNS $VCPE --"
 
 echo "## 2. Ejecutando instancia de softflowd e inyectando PCAP con tráfico de criptominado conocido"
-$CPE_EXEC /usr/sbin/softflowd -d -r capture+crypto+type2+8+vlan.pcap -v 9 -t maxlife=30s -T ether -n goflow2-1.default.svc.cluster.local:9995 &
+$CPE_EXEC /usr/sbin/softflowd -d -r /etc/softflowd/capture+crypto+type2+8+vlan.pcap -v 9 -t maxlife=30s -T ether -n goflow2-1.default.svc.cluster.local:9995 &
 sleep 10
