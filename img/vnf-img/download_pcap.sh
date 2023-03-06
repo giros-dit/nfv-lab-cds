4cee369bae361653bda01f9f9b6fdede73af4b37
martinezgarciadavid <94689492+martinezgarciadavid@users.noreply.github.com>
2023-03-06 11:56:05 +0100

    Update download_pcap.sh

--- img/vnf-img/download_pcap.sh
+++ img/vnf-img/download_pcap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://github.com/martinezgarciadavid/nfv-lab-cds/releases/download/v1.0.0/capture+crypto+type2+8+vlan.pcap
+wget https://github.com/giros-dit/nfv-lab-cds/releases/download/v1.0.0/capture+crypto+type2+8+vlan.pcap
